a332864f1ab1b666031ecd504f64c8143fd478b0
Stephen <stephen@behnfeldt.pro>
2024-01-07 09:28:58 -0500

    Lesson 29) Single Post Convert the static HTML in single.php to dynamic content.

--- single.php
+++ single.php
@@ -1,64 +1,59 @@
-<html>
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-    <link rel="stylesheet" href="<?php bloginfo( 'stylesheet_url' ) ?>">
-    <title>PhotoGenik</title>
-</head>
+<?php global $id; ?>
+<html <?php language_attributes(); ?>>
+<?php get_template_part( 'head' ); ?>
 
-<body>
-
-<header class="w3-container w3-teal">
-    <div class="w3-row">
-        <div class="w3-col m9 l9">
-            <h1><span>Photo</span>Genik</h1>
-        </div>
-        <div class="w3-col m3 l3">
-            <input type="text" class="w3-input" placeholder="Search...">
-        </div>
-    </div>
-</header>
+<body <?php body_class(); ?>>
+<?php get_template_part( 'header' ); ?>
 
 <div class="w3-row">
-    <!-- Sidebar -->
-    <div class="w3-col m3 l3">
-        <ul class="w3-ul">
-            <li><a href="#">Nature</a></li>
-            <li><a href="#">Animals</a></li>
-            <li><a href="#">Objects</a></li>
-            <li><a href="#">People</a></li>
-            <li><a href="#">Abstract</a></li>
-        </ul>
-    </div>
+    <?php get_template_part( 'sidebar' ); ?>
 
+    <!--    Main -->
     <main class="w3-col m9 l9">
-        <article class="post">
-            <h1>Photo Title</h1>
-            <p class="meta">
-                Posted at 2:00pm on 03-29-2023 by Stephen.
-            </p>
-            <hr/>
-            <img class="pic w3-animate-right" src="<?= get_template_directory_uri() ?>/images/2.jpg" alt="">
-            <div class="w3-row">
-                <div class="w3-col l2"></div>
-                <a class="w3-btn w3-red" href="/">Back</a>
-                <div class="w3-col l10"></div>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus dolorum ea earum exercitationem
-                    reiciendis! Cum, ipsum iusto omnis reprehenderit repudiandae vero voluptatibus! Esse eum illo magni
-                    nostrum repudiandae sequi, sit.</p>
-            </div>
+        <?php if ( have_posts() ): ?>
+            <?php while ( have_posts() ) : the_post(); ?>
+                <article class="post">
+                    <p class="meta">
+                        Posted at <?php the_time(); ?>
+                        on <?php the_date(); ?>
+                        by <a
+                            href="<?php echo get_author_posts_url( get_the_author_meta( 'ID' ) ); ?>"><?php the_author(); ?></a>
+                    </p>
+                    <hr>
 
-        </article>
+                    <?php if ( has_post_thumbnail() ) : ?>
+                        <div class="post-thumbnail">
+                            <h2><?php the_title(); ?></h2>
+
+                            <?php
+                            $attr = [
+                                'class' => 'w3-animate-right'
+                            ]
+                            ?>
+                            <?php echo get_the_post_thumbnail( $id, 'large', $attr ); ?>
+                        </div>
+                    <?php endif; ?>
+
+
+                    <div class="w3-row">
+                        <div class="w3-col l2">
+                            <br>
+                            <a href="<?php echo site_url(); ?>" class="w3-btn w3-red">Back</a>
+                        </div>
+                        <div class="w3-col l10">
+                            <h1><?php the_title(); ?></h1>
+                            <?php the_content(); ?>
+                        </div>
+                    </div>
+                </article>
+            <?php endwhile; ?>
+        <?php else: ?>
+            <?php echo wpautop( 'Sorry, no posts' ); ?>
+        <?php endif; ?>
     </main>
 </div>
 
-
-<footer>
-    <p>PhotoGenik &copy; 2023</p>
-</footer>
+<?php get_template_part( 'footer' ); ?>
 
 </body>
 </html>
