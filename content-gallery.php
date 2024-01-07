dc8eac317bc9233b87173adfc0dba5bb7ad7c263
Stephen <stephen@behnfeldt.pro>
2024-01-07 08:48:50 -0500

    Lesson 27) Displaying Post Content Create template parts "content.php" and "content-gallery.php" and use them to display posts on the Posts page.

--- content-gallery.php
+++ content-gallery.php
@@ -1,1 +1,18 @@
-PHOTO
+<?php global $id; ?>
+<div class="w3-col m4 l4 pic">
+    <?php if ( has_post_thumbnail() ) : ?>
+        <div class="post-thumbnail">
+            <h2><?php the_title(); ?></h2>
+
+            <?php
+            $attr = [
+                'class' => 'w3-animate-opacity w3-hover-opacity'
+            ]
+            ?>
+            <a href="<?php the_permalink(); ?>">
+                <?php echo get_the_post_thumbnail( $id, 'large', $attr ); ?>
+            </a>
+        </div>
+    <?php endif; ?>
+    <?php the_content(); ?>
+</div>
--- index.php
+++ index.php
@@ -7,20 +7,16 @@
 <div class="w3-row">
     <?php get_template_part( 'sidebar' ); ?>
 
-    <?php if ( have_posts() ): ?>
+    <!--    Main -->
     <main class="w3-col m9 l9">
-        <div class="w3-row">
-    <?php $count = 0; ?>
-        <?php while ( have_posts() ) : the_post(); $count++;?>
-            <?php get_template_part( 'content', get_post_format() ); ?>
-        <?php endwhile; ?>
-        </div>
-        <h1>Count: <?php echo $count; ?></h1>
+        <?php if ( have_posts() ): ?>
+            <?php while ( have_posts() ) : the_post(); ?>
+                <?php echo get_template_part( 'content', get_post_format() ); ?>
+            <?php endwhile; ?>
+        <?php else: ?>
+            <?php echo wpautop( 'Sorry, no posts' ); ?>
+        <?php endif; ?>
     </main>
-    <?php else: ?>
-        <?php echo wpautop( 'Sorry, no posts' ); ?>
-    <?php endif; ?>
-
 </div>
 
 <?php get_template_part( 'footer' ); ?>
